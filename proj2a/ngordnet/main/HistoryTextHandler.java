6b08eec550c099ae85b7cdff49bea6e41b9b51a2
Quang Nguyen <nhnquang11@gmail.com>
2023-03-21 01:50:38 +0700

    'Re'finish setting up web server

--- proj2a/ngordnet/main/HistoryTextHandler.java
+++ proj2a/ngordnet/main/HistoryTextHandler.java
@@ -1,2 +1,34 @@
-package ngordnet.main;public class HistoryTextHandler {
+package ngordnet.main;
+
+import ngordnet.hugbrowsermagic.NgordnetQuery;
+import ngordnet.hugbrowsermagic.NgordnetQueryHandler;
+import ngordnet.ngrams.NGramMap;
+import ngordnet.ngrams.TimeSeries;
+
+import java.util.List;
+
+public class HistoryTextHandler extends NgordnetQueryHandler {
+    private NGramMap ngm;
+    public HistoryTextHandler(NGramMap ngm) {
+        this.ngm = ngm;
+    }
+
+    @Override
+    public String handle(NgordnetQuery q) {
+        List<String> words = q.words();
+        int startYear = q.startYear();
+        int endYear = q.endYear();
+
+        StringBuilder response = new StringBuilder();
+        for (String word: words) {
+            response.append("   ");
+            response.append(word);
+            response.append(": ");
+            TimeSeries ts = ngm.weightHistory(word, startYear, endYear);
+            response.append(ts.toString());
+            response.append("\n");
+        }
+
+        return response.toString();
+    }
 }
